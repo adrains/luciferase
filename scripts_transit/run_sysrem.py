Corrected incorrect simulation SNR scaling, updates to SYSREM visualisation and residual preparation.

scripts_transit/run_sysrem.py
@@ -78,6 +78,17 @@ bad_px_mask_3D = np.tile(
 for spec_i in ss.segments_to_mask_completely:
     bad_px_mask_3D[:,spec_i,:] = np.full((n_phase, n_px), True)
 
+# DEBUGGING: Drop segments
+if ss.do_drop_segments:
+    # Slice arrays
+    waves = waves[ss.segments_to_keep, :]
+    fluxes_norm = fluxes_norm[:, ss.segments_to_keep,]
+    sigmas_norm = sigmas_norm[:, ss.segments_to_keep, :]
+    bad_px_mask_3D = bad_px_mask_3D[:,ss.segments_to_keep,]
+
+    # Update dimensions
+    (n_phase, n_spec, n_px) = fluxes_norm.shape
+
 #------------------------------------------------------------------------------
 # Import planet spectra
 #------------------------------------------------------------------------------
@@ -141,7 +152,7 @@ for spec_i in range(n_spec):
     print("-"*80, fmt_txt, "-"*80,sep="")
     
     # Skip entirely nan segments
-    if spec_i in ss.segments_to_mask_completely:
+    if spec_i in ss.segments_to_mask_completely and not ss.do_drop_segments:
         print("\tSkipping...\n")
         continue
 
@@ -153,11 +164,15 @@ for spec_i in range(n_spec):
         mjds=transit_info_list[ss.transit_i]["mjd_mid"].values,
         tolerance=ss.sysrem_convergence_tol,
         max_converge_iter=ss.sysrem_max_convergence_iter,
-        diff_method=ss.sysrem_diff_method,)
+        diff_method=ss.sysrem_diff_method,
+        sigma_threshold_phase=ss.sigma_threshold_phase,
+        sigma_threshold_spectral=ss.sigma_threshold_spectral,)
     
     resid_all[:,:,spec_i,:] = resid
 
 tplt.plot_sysrem_residuals(waves, resid_all)
+
+#assert False
 
 #------------------------------------------------------------------------------
 # Run Cross Correlation

scripts_transit/simulate_transit.py
@@ -40,6 +40,16 @@ scale_components = []
 
 line = "-"*80
 
+# Print summary
+print(line, "\nSimulation Settings\n", line, sep="")
+if ss.target_snr is None:
+    print("\tSNR\t\tinf")
+else:
+    print("\tSNR\t\t{:0.0f}".format(ss.target_snr))
+print("\tBoost Fac\t{:0.0f}x".format(ss.planet_transmission_boost_fac))
+print("\tBlaze Corr\t{}".format(ss.correct_for_blaze))
+print("\tScale Vec\t{}\n".format(ss.scale_vector_method))
+
 # Run separately for each transit
 for transit_i in range(ss.n_transit):
     print(line, "\nModelling transit #{}\n".format(transit_i), line, sep="")
@@ -111,10 +121,14 @@ for trans_i in range(ss.n_transit):
 # Save results to fits
 # -----------------------------------------------------------------------------
 # Construct save file name
-if ss.target_snr is None or ss.target_snr.upper() == "NONE":
-    target_snr = np.inf
-else:
+if type(ss.target_snr) == float or type(ss.target_snr) == int:
     target_snr = ss.target_snr
+
+elif ss.target_snr is None or ss.target_snr.upper() == "NONE":
+    target_snr = np.inf
+
+else:
+    raise Exception("Something went wrong")
 
 fn_label = "{}_trans_boost_x{:0.0f}_SNR_{:0.0f}".format(
     ss.label, ss.planet_transmission_boost_fac, target_snr)
@@ -123,7 +137,7 @@ fn_label = "{}_trans_boost_x{:0.0f}_SNR_{:0.0f}".format(
 tu.save_transit_info_to_fits(
     waves=waves,
     obs_spec_list=model_flux_list,
-    sigmas_list=model_sigma_list,
+    sigmas_list=model_flux_list,
     n_transits=ss.n_transit,
     detectors=det,
     orders=orders,

transit/plotting.py
@@ -7,8 +7,8 @@ from matplotlib import cm
 import transit.utils as tu
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.ticker as plticker
-import matplotlib.colors as colors
 from tqdm import tqdm
+from astropy.stats import sigma_clip
 
 def plot_iteration():
     """
@@ -512,7 +512,11 @@ def plot_epoch_model_comp(
 def plot_sysrem_residuals(
     waves,
     resid,
-    fig_size=(18,6)):
+    fig_size=(18,6),
+    do_sigma_clip=False,
+    sigma_upper=5,
+    sigma_lower=5,
+    max_iterations=5,):
     """Function to plot a grid of residuals as output from SYSREM. The grid has
     n_rows = n_sysrem_iter, and n_cols = n_spec.
 
@@ -541,31 +545,45 @@ def plot_sysrem_residuals(
         figsize=fig_size,)
     
     plt.subplots_adjust(
-        left=0.05, bottom=0.1, right=0.95, top=0.95, wspace=0.1)
+        left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.1)
 
     for sr_iter_i in range(n_sysrem_iter):
-        # Set colourbar normalisation for this SYSREM iteration
-        vmin = np.nanmin(resid[sr_iter_i])
-        vmax = np.nanmax(resid[sr_iter_i])
-
         desc = "Plotting SYSREM resid for iter #{}".format(sr_iter_i)
 
         for spec_i in tqdm(range(n_spec), leave=False, desc=desc):
+            # [Optional] Sigma clip the noise away to aid in visualisation
+            if do_sigma_clip:
+                resid_ma = sigma_clip(
+                    data=resid[sr_iter_i, :, spec_i],
+                    sigma_lower=sigma_lower,
+                    sigma_upper=sigma_upper,
+                    maxiters=max_iterations,)
+                
+                resid_ith = resid_ma.data
+                mask = resid_ma.mask
+                resid_ith[mask] = np.nan
+
+            # Otherwise plot residuals as is
+            else:
+                resid_ith = resid[sr_iter_i, :, spec_i]
+            
             # [x_min, x_max, y_min, y_max]
             extent = [np.min(waves[spec_i]), np.max(waves[spec_i]), n_phase, 0]
-           
+            
+            # Grab axis handle for convenience
             axis = axes[sr_iter_i, spec_i]
 
             cmap = axis.imshow(
-                X=resid[sr_iter_i, :, spec_i],
+                X=resid_ith,
                 aspect="auto",
                 interpolation="none",
-                vmin=vmin,
-                vmax=vmax,
+                #vmin=vmin,
+                #vmax=vmax,
                 extent=extent,
                 #norm=colors.LogNorm(vmin=vmin, vmax=vmax,))
-                #norm=colors.PowerNorm(gamma=5),
-                cmap="PRGn")
+                #norm=colors.PowerNorm(gamma=2),
+                cmap="plasma",)
+                #cmap="PRGn")
 
             axis.tick_params(axis='both', which='major', labelsize="x-small")
             axis.xaxis.set_major_locator(plticker.MultipleLocator(base=4))
@@ -574,24 +592,28 @@ def plot_sysrem_residuals(
 
             # Only show xticks on the bottom
             if sr_iter_i != n_sysrem_iter-1:
-                 axes[sr_iter_i, spec_i].set_xticks([])
+                axis.set_xticks([])
 
+            # Only show Y labels on the left
+            if spec_i == 0:
+                axis.set_ylabel("Iter #{}".format(sr_iter_i))
+            
             # Only show yticks on the left
-            if spec_i != 0:
-                 axes[sr_iter_i, spec_i].set_yticks([])
+            else:
+                axis.set_yticks([])
+            
+            # Show just one colour bar per iteration
+            #ticks_norm = np.arange(0,1.25,0.25)
+            #ticks_rescaled = (ticks_norm * (vmax-vmin) + vmin).astype(int)
 
-        # Show just one colour bar per iteration
-        ticks_norm = np.arange(0,1.25,0.25)
-        ticks_rescaled = (ticks_norm * (vmax-vmin) + vmin).astype(int)
+            #fig.subplots_adjust(right=0.8)
+            divider = make_axes_locatable(axis)
+            cb_ax = divider.append_axes("right", size="5%", pad="1%")
 
-        #fig.subplots_adjust(right=0.8)
-        divider = make_axes_locatable(axis)
-        cb_ax = divider.append_axes("right", size="10%", pad="10%")
-
-        #cb_ax = fig.add_axes([0.9, 0.05, 0.025, 0.9])  # [L, B, W, H]
-        cbar = fig.colorbar(cmap, cax=cb_ax, aspect=5)
-        #cbar.ax.set_yticklabels(ticks_rescaled)
-        #cbar.set_label("Mean Wavelength of Spectral Segment")
+            #cb_ax = fig.add_axes([0.9, 0.05, 0.025, 0.9])  # [L, B, W, H]
+            cbar = fig.colorbar(cmap, cax=cb_ax, aspect=5)
+            #cbar.ax.set_yticklabels(ticks_rescaled)
+            #cbar.set_label("Mean Wavelength of Spectral Segment")
 
 
 def plot_sysrem_cc_1D(

transit/simulator.py
@@ -1119,11 +1119,9 @@ def simulate_transit_single_epoch(
     r_tel_prim,
     r_tel_cen_ob,
     throughput_json_path,
-    target_snr,
     do_equid_lambda_resample=True,
     fill_throughput_value=0,
     planet_transmission_boost_fac=1,
-    correct_for_blaze=True,
     constant_gain=2,):
     """Simulate a single epoch of a planet during transit by modelling the
     stellar, planetary, and telluric components. This function takes into
@@ -1212,13 +1210,6 @@ def simulate_transit_single_epoch(
         Filepath to the CRIRES+ JSON file containing instrument transfer
         functions as a function of wavelength.
 
-    target_snr: float or None
-        Target SNR such that the median of the brightest spectral segment has
-        SNR = target_snr. Uncertainties are then computed as Poisson
-        uncertainties, and noise applied by sampling from a normal distribution
-        with mean as the scale signal, and width the uncertainties. If set to
-        None, the spectrum is not rescaled and no noise is applied.
-
     do_equid_lambda_resample: boolean, default: True
         Whether to resample wavelength and flux arrays onto a new wavelength
         scale with equidistant lambda sampling prior to broadening. This is
@@ -1231,11 +1222,6 @@ def simulate_transit_single_epoch(
     planet_transmission_boost_fac: float, default: 1
         Whether to artificially boost the strength of planetary absorption by
         some factor for testing. By default set to 1 (i.e. disabled).
-
-    correct_for_blaze: boolean, default: True
-        Whether or not to correct for the effect of the blaze function once
-        we've simulated the complete transfer function and computed 
-        uncertainties.
 
     constant_gain: float, default: 2
         Detector gain to use. TODO: Update this to be more realistic.
@@ -1419,55 +1405,6 @@ def simulate_transit_single_epoch(
     flux_counts = flux_counts_per_sec * transit_epoch["exptime_sec"]
 
     # -------------------------------------------------------------------------
-    # Add noise [Optional], compute uncertainties
-    # -------------------------------------------------------------------------
-    print("\tComputing uncertainties...")
-    # If we've been provided a target SNR, apply noise. We benchmark to be the
-    # median for the order with the most signal and find the scale factor
-    # needed to adjust such that the Poisson uncertainty gives us the desired
-    # SNR, then scale the entire spectrum by that value.
-    if target_snr is not None:
-        # Flux has shape [n_spec, n_wave]
-        max_counts = np.max(np.nanmedian(flux_counts, axis=1))
-
-        # Compute the scale factor as:
-        #  snr_target = counts_orig * scale_fac / sqrt(counts_orig * scale_fac)
-        #  scale_fac = snr_target^2 / counts_orig
-        sf = target_snr**2 / max_counts
-
-        # Scale entire spectrum
-        flux_counts *= sf
-
-        # Compute the uncertainties
-        sigma_counts = flux_counts**0.5
-
-        # Assuming Gaussian uncertainties, resample the spectra to add noise
-        flux_counts = np.random.normal(loc=flux_counts, scale=sigma_counts)
-
-        # Clip this to be >= 0
-        flux_counts = np.clip(flux_counts, a_min=0, a_max=None,)
-
-    # Otherwise no noise, scale factor = 1, Poisson uncertainties
-    else:
-        sf = 1
-        sigma_counts = flux_counts**0.5
-
-    # -------------------------------------------------------------------------
-    # [Optional] Remove the effect of the blaze
-    # -------------------------------------------------------------------------
-    # So that we properly calculate and propagate uncertainties, if we want to
-    # remove the effect of the blaze we do that here (rather than just not
-    # applying it to begin with). By doing it this way our rescaled spectrum
-    # retains the same SNR.
-    if correct_for_blaze:
-        flux_counts, sigma_counts = remove_instrumental_blaze_function(
-            wave=wave_observed,
-            flux=flux_counts,
-            sigma=sigma_counts,
-            throughput_json_path=throughput_json_path,
-            fill_throughput_value=fill_throughput_value,)
-    
-    # -------------------------------------------------------------------------
     # Prepare component spectra
     # -------------------------------------------------------------------------
     # Finally we prepare our 'component' spectra--the uncombined stellar flux,
@@ -1485,16 +1422,8 @@ def simulate_transit_single_epoch(
         wave=wave_observed,
         flux=flux_component,
         gain=constant_gain,) * transit_epoch["exptime_sec"]
-    
-    if correct_for_blaze:
-        flux_component_counts, _ = remove_instrumental_blaze_function(
-            wave=wave_observed,
-            flux=flux_component_counts,
-            sigma=np.zeros_like(flux_component_counts),
-            throughput_json_path=throughput_json_path,
-            fill_throughput_value=fill_throughput_value,)
 
-    component_spectra["stellar_flux"] = flux_component_counts * sf
+    component_spectra["stellar_flux"] = flux_component_counts
     
     # Tellurics
     component_spectra["telluric_tau"] = tau_obs
@@ -1502,8 +1431,19 @@ def simulate_transit_single_epoch(
     # Planet
     component_spectra["planet_trans"] = planet_trans_obs
 
+    # -------------------------------------------------------------------------
+    # Final Sanity Check
+    # -------------------------------------------------------------------------
+    # Something has gone wrong if we have an all nan vector, so raise an
+    # exception just in case.
+    if (np.sum(~np.isnan(flux_counts)) == 0
+        or np.sum(~np.isnan(component_spectra["stellar_flux"])) == 0
+        or np.sum(~np.isnan(component_spectra["telluric_tau"])) == 0
+        or np.sum(~np.isnan(component_spectra["planet_trans"])) == 0):
+        raise ValueError("All nan vector detected!")
+
     # All done
-    return flux_counts, sigma_counts, component_spectra
+    return flux_counts, component_spectra
 
 
 def simulate_transit_multiple_epochs(
@@ -1727,7 +1667,7 @@ def simulate_transit_multiple_epochs(
     # Loop over all phases and determine fluxes
     for epoch_i, transit_epoch in transit_info.iterrows():
         print("Simulating epoch {}...".format(epoch_i))
-        flux_counts, sigma_counts, _ = simulate_transit_single_epoch(
+        flux_counts, _ = simulate_transit_single_epoch(
             wave_observed=wave_observed,
             wave_marcs=wave_marcs,
             fluxes_marcs=fluxes_marcs,
@@ -1743,14 +1683,68 @@ def simulate_transit_multiple_epochs(
             r_tel_prim=r_tel_prim,
             r_tel_cen_ob=r_tel_cen_ob,
             do_equid_lambda_resample=do_equid_lambda_resample,
-            target_snr=target_snr,
             throughput_json_path=throughput_json_path,
             fill_throughput_value=fill_throughput_value,
-            planet_transmission_boost_fac=planet_transmission_boost_fac,
-            correct_for_blaze=correct_for_blaze,)
+            planet_transmission_boost_fac=planet_transmission_boost_fac,)
 
         fluxes_model_all[epoch_i] = flux_counts
-        sigma_model_all[epoch_i] = sigma_counts
+
+    # -------------------------------------------------------------------------
+    # Add noise [Optional], compute uncertainties
+    # -------------------------------------------------------------------------
+    print("\tComputing uncertainties...")
+    # If we've been provided a target SNR, apply noise. We need to do this here
+    # once we've generated spectra for the entire transit so that our SNR 
+    # scaling preserves flux changes over the transit. We benchmark to the
+    # median for the order with the most signal and find the scale factor
+    # needed to adjust such that the Poisson uncertainty gives us the desired
+    # SNR, then scale the entire spectrum by that value.
+    if target_snr is not None:
+        print("Applying noise...")
+        # Flux has shape [n_phase, n_spec, n_wave], compute median flux for
+        # each order over the entire time series and then scale to the order
+        # with the most flux.
+        median_order_fluxes = \
+            np.nanmedian(np.nanmedian(fluxes_model_all, axis=2), axis=0)
+        max_counts = np.nanmax(median_order_fluxes)
+
+        # Compute the scale factor as:
+        #  snr_target = counts_orig * scale_fac / sqrt(counts_orig * scale_fac)
+        #  scale_fac = snr_target^2 / counts_orig
+        sf = target_snr**2 / max_counts
+
+        # Scale entire spectrum
+        fluxes_model_all *= sf
+
+        # Compute the uncertainties
+        sigma_counts = fluxes_model_all**0.5
+
+        # Assuming Gaussian uncertainties, resample the spectra to add noise
+        fluxes_model_all = \
+            np.random.normal(loc=fluxes_model_all, scale=sigma_counts)
+
+        # Clip this to be >= 0
+        fluxes_model_all = np.clip(fluxes_model_all, a_min=0, a_max=None,)
+
+    # Otherwise no noise, scale factor = 1, Poisson uncertainties
+    else:
+        sf = 1
+        sigma_model_all = fluxes_model_all**0.5
+
+    # -------------------------------------------------------------------------
+    # [Optional] Remove the effect of the blaze
+    # -------------------------------------------------------------------------
+    # So that we properly calculate and propagate uncertainties, if we want to
+    # remove the effect of the blaze we do that here (rather than just not
+    # applying it to begin with). By doing it this way our rescaled spectrum
+    # retains the same SNR.
+    if correct_for_blaze:
+        fluxes_model_all, sigma_model_all = remove_instrumental_blaze_function(
+            wave=wave_observed,
+            flux=fluxes_model_all,
+            sigma=sigma_model_all,
+            throughput_json_path=throughput_json_path,
+            fill_throughput_value=fill_throughput_value,)
 
     # -------------------------------------------------------------------------
     # Prepare component spectra
@@ -1772,7 +1766,7 @@ def simulate_transit_multiple_epochs(
     rest_frame_epoch["v_y_start"] = delta_rv
     rest_frame_epoch["v_y_end"] = 0
     
-    _, _, component_vectors = simulate_transit_single_epoch(
+    _, component_vectors = simulate_transit_single_epoch(
             wave_observed=wave_observed,
             wave_marcs=wave_marcs,
             fluxes_marcs=fluxes_marcs,
@@ -1788,11 +1782,23 @@ def simulate_transit_multiple_epochs(
             r_tel_prim=r_tel_prim,
             r_tel_cen_ob=r_tel_cen_ob,
             do_equid_lambda_resample=do_equid_lambda_resample,
-            target_snr=target_snr,
             throughput_json_path=throughput_json_path,
             fill_throughput_value=fill_throughput_value,
-            planet_transmission_boost_fac=planet_transmission_boost_fac,
-            correct_for_blaze=correct_for_blaze,)
+            planet_transmission_boost_fac=planet_transmission_boost_fac,)
+
+    # [Optional] Apply blaze correction to component spectra
+    if correct_for_blaze:
+        flux_component_counts, _ = remove_instrumental_blaze_function(
+            wave=wave_observed,
+            flux=component_vectors["stellar_flux"],
+            sigma=np.zeros_like(component_vectors["stellar_flux"]),
+            throughput_json_path=throughput_json_path,
+            fill_throughput_value=fill_throughput_value,)
+
+        component_vectors["stellar_flux"] = flux_component_counts
+
+    # Scale component spectra
+    component_vectors["stellar_flux"] *= sf
 
     # Add in scale vector
     component_vectors["scale_vector"] = scale_vector

transit/sysrem.py
@@ -17,7 +17,8 @@ def clean_and_compute_initial_resid(
     mjds,
     sigma_threshold_phase=6.0,
     sigma_threshold_spectral=6.0,
-    n_max_phase_bad_px=5,):
+    n_max_phase_bad_px=5,
+    do_clip_spectral_dimension=False,):
     """Function to clean a spectral data cube prior to running SYSREM. We sigma
     clip along the phase dimension (i.e. the time series of each pixel) as well
     as along the spectral dimension. 
@@ -71,16 +72,18 @@ def clean_and_compute_initial_resid(
     # Sigma clip along phase dimension. Note that here we're doing both upper
     # *and* lower bound clipping.
     # TODO: we eventually want to interpolate along the spectral dimension
+    
     sc_mask_phase = np.full_like(bad_px_mask_init, False)
 
     for px_i in range(n_px):
-        # Compute mask for px i
+        # Sigma clip along the phase dimension to compute the bad pixel mask 
+        # for px i
         bad_phase_mask = sigma_clip(
             data=flux[:,px_i],
             sigma=sigma_threshold_phase,).mask
         
         # If we have more than the threshold number of bad px, mask out the
-        # entire column
+        # entire column (i.e. mask out px_i)
         if np.sum(bad_phase_mask) > n_max_phase_bad_px:
             sc_mask_phase[:,px_i] = True
 
@@ -110,16 +113,20 @@ def clean_and_compute_initial_resid(
             
             # Update the bad px mask to just have nan values (i.e. filled)
             sc_mask_phase[:,px_i] = np.isnan(flux[:,px_i])
+
+            # HACK: this should always be 0 right?
+            assert np.sum(np.isnan(flux[:,px_i])) == 0
         
     # Sigma clip along the spectral dimension. Note that since we're clipping
     # along the spectral dimension, we only clip the *upper* bounds so as to
     # not remove absorption features.
     sc_mask_spec = np.full_like(bad_px_mask_init, False)
 
-    for phase_i in range(n_phase):
-        sc_mask_spec[phase_i,:] = sigma_clip(
-            data=flux[phase_i,:],
-            sigma_upper=sigma_threshold_spectral,).mask
+    if do_clip_spectral_dimension:
+        for phase_i in range(n_phase):
+            sc_mask_spec[phase_i,:] = sigma_clip(
+                data=flux[phase_i,:],
+                sigma_upper=sigma_threshold_spectral,).mask
         
     # Combine bad px masks
     bad_px_mask = np.logical_or(
@@ -143,7 +150,9 @@ def run_sysrem(
     mjds,
     tolerance=1E-6,
     max_converge_iter=100,
-    diff_method="max",):
+    diff_method="max",
+    sigma_threshold_phase=6.0,
+    sigma_threshold_spectral=6.0,):
     """Function to run the iterative SYSREM algorithm on a set of fluxes and 
     uncertainties. Note that this only runs on a single spectral segment at a
     time.
@@ -220,7 +229,12 @@ def run_sysrem(
 
     # Use bad px mask to clean input array and compute initial residual vector
     resid_init, flux, e_flux = clean_and_compute_initial_resid(
-        spectra, e_spectra, bad_px_mask, mjds)
+        spectra=spectra,
+        e_spectra=e_spectra,
+        bad_px_mask_init=bad_px_mask,
+        mjds=mjds,
+        sigma_threshold_phase=sigma_threshold_phase,
+        sigma_threshold_spectral=sigma_threshold_spectral,)
 
     # Store median subtracted residuals
     resid_all[0] = resid_init