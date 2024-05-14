Updates to cleaning function, both CC scripts nominally working, fixed sign on phase for Kp-Vsys maps.

scripts_transit/run_cc.py
@@ -3,12 +3,14 @@ exoplanet spectrum.
 """
 import numpy as np
 import transit.utils as tu
-import transit.simulator as sim
 import transit.sysrem as sr
 import transit.plotting as tplt
 import astropy.units as u
+import luciferase.utils as lu
 from astropy import constants as const
 from PyAstronomy.pyasl import instrBroadGaussFast
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 #------------------------------------------------------------------------------
 # Settings
@@ -23,13 +25,6 @@ ss = tu.load_yaml_settings(simulation_settings_file)
 waves, fluxes_list, sigmas_list, det, orders, transit_info_list, syst_info = \
     tu.load_transit_info_from_fits(ss.save_path, ss.label, ss.n_transit)
 
-# Import normalised fluxes
-fluxes_norm, sigmas_norm, bad_px, poly_coeff = tu.load_normalised_spectra_from_fits(
-    fits_load_dir=ss.save_path,
-    label=ss.label,
-    n_transit=ss.n_transit,
-    transit_i=ss.transit_i,)        # TODO: this will eventually be in a loop
-
 # Grab shape
 (n_phase, n_spec, n_px) = fluxes_list[ss.transit_i].shape
 
@@ -37,20 +32,10 @@ fluxes_norm, sigmas_norm, bad_px, poly_coeff = tu.load_normalised_spectra_from_f
 resid_all = tu.load_sysrem_residuals_from_fits(
     ss.save_path, ss.label, ss.n_transit, ss.transit_i,)
 
-# Mask out entire segments
-for spec_i in ss.segments_to_mask_completely:
-    bad_px[:,spec_i,:] = np.full((n_phase, n_px), True)
+n_sysrem_iter = resid_all.shape[0]
 
-# DEBUGGING: Drop segments
-if ss.do_drop_segments:
-    # Slice arrays
-    waves = waves[ss.segments_to_keep, :]
-    fluxes_norm = fluxes_norm[:, ss.segments_to_keep,]
-    sigmas_norm = sigmas_norm[:, ss.segments_to_keep, :]
-    bad_px_mask_3D = bad_px[:,ss.segments_to_keep,]
-
-    # Update dimensions
-    (n_phase, n_spec, n_px) = fluxes_norm.shape
+planet_rvs = \
+    transit_info_list[ss.transit_i]["delta"].values*const.c.cgs.to(u.km/u.s)
 
 #------------------------------------------------------------------------------
 # Import telluric spectra
@@ -67,24 +52,19 @@ telluric_wave, _, _, telluric_trans = tu.load_telluric_spectrum(
 #------------------------------------------------------------------------------
 # Load in petitRADRTRANS datacube of templates. These templates will be in
 # units of R_earth as a function of wavelength.
-templ_wave, templ_spec_all, templ_info = \
-    tu.load_transmission_templates_from_fits(fits_file=ss.template_fits)
+wave_p, spec_p_all, templ_info = tu.load_transmission_templates_from_fits(
+    fits_file=ss.template_fits,
+    min_wl_nm=16000,
+    max_wl_nm=30000,)
 
 # Clip edges to avoid edge effects introduced by interpolation
-templ_spec_all = templ_spec_all[:,10:-10]
-templ_wave = templ_wave[10:-10] / 10
+templ_spec_all = spec_p_all[:,10:-10]
+wave_p = wave_p[10:-10] / 10
 
 molecules = templ_info.columns.values
 
 # Pick a template
-templ_i = 0     # CO model
-
-# The datacube spectra are at R~200,000, so we need to further downsample
-templ_spec_instr = instrBroadGaussFast(
-        wvl=templ_wave,
-        flux=templ_spec_all[templ_i],
-        resolution=100000,
-        equid=True,)
+templ_i = 1     # H2O model
 
 # Convert to a transmission spectrum
 r_e = const.R_earth.si.value
@@ -93,91 +73,71 @@ r_odot = const.R_sun.si.value
 rp = syst_info.loc["r_planet_rearth", "value"] 
 rs = syst_info.loc["r_star_rsun", "value"] * r_odot / r_e
 
-trans_planet_instr = 1 - ((templ_spec_instr + rp)**2  / rs**2)
+trans_planet = 1 - ((templ_spec_all[templ_i] + rp)**2  / rs**2)
+
+# Compute planet 'continuum' to normalise by
+planet_cont = instrBroadGaussFast(
+        wvl=wave_p,
+        flux=trans_planet,
+        resolution=300,
+        equid=True,)
 
 # [Optional] For testing, we can use the telluric vector for cross correlation
 if ss.cc_with_telluric:
+    print("Cross correlating with telluric template.")
     wave_template = telluric_wave
     spectrum_template = telluric_trans
 
+# [Optional] Or we can use a stellar spectrum
+elif ss.cc_with_stellar:
+    print("Cross correlating with stellar template.")
+    wave_stellar, spec_stellar = lu.load_plumage_template_spectrum(
+        template_fits=ss.stellar_template_fits,
+        do_convert_air_to_vacuum_wl=False,)
+    
+    wave_template = wave_stellar
+    spectrum_template = spec_stellar
+
 # Otherwise run on a planet spectrum
 else:
-    wave_template = templ_wave
-    spectrum_template = trans_planet_instr
+    print("Cross correlating with planet template.")
+    wave_template = wave_p
+    spectrum_template = trans_planet / planet_cont
 
 #------------------------------------------------------------------------------
-# Run Cross Correlation
+# Cross-correlation
 #------------------------------------------------------------------------------
-cc_rvs, cc_values = sr.cross_correlate_sysrem_resid(
+cc_rvs, ccv_per_spec, ccv_global = sr.cross_correlate_sysrem_resid(
     waves=waves,
     sysrem_resid=resid_all,
-    sigma_spec=sigmas_norm,
     template_wave=wave_template,
     template_spec=spectrum_template,
     cc_rv_step=ss.cc_rv_step,
     cc_rv_lims=ss.cc_rv_lims,
     interpolation_method="cubic",)
 
-# ------
-# Correct the per-segment cc
-# ------
-# Subtract minimum value for each [sysrem_i, phase_i, spec_i]
-# HACK: Currently the cross-correlation peak isn't hugely significant over the
-# 'background', meaning that to see anything we need to subtract the median
-# value.....which isn't correct, so this will be removed once we figure out
-# what is going on.
-cc_values_median = np.broadcast_to(
-    np.nanmedian(cc_values, axis=3)[:,:,:,None], cc_values.shape)
-cc_vals_subbed = cc_values - cc_values_median
-
-# ------
-# Correct the total cc
-# ------
-cc_values_total = np.nansum(cc_values, axis=2)[:,:,None,:]
-cc_values_total_median = np.broadcast_to(
-    np.nanmedian(cc_values_total, axis=3)[:,:,:,None], cc_values_total.shape)
-cc_vals_total_subbed = cc_values_total - cc_values_total_median
-
-# Combine all segments into single mean CCF
-#cc_values_mean = np.nanmean(cc_values, axis=2)
-#cc_values_sum = np.nansum(cc_values, axis=2)
-
-# Plot cross RV vs correlation value in a separate panel in for each SYSREM
-# iteration. We colour code each line by the phase number.
-#tplt.plot_sysrem_cc_1D(cc_rvs, cc_values_mean,)
-
-# Instead plot the cross correlation as a 2D map (RV vs phase) where the colour
-# bar is the cross correlation value.
-planet_rvs = \
-    transit_info_list[ss.transit_i]["delta"].values*const.c.cgs.to(u.km/u.s)
-
-# First plot the cross-correlation one order at a time
+# Plot cross-correlation
 tplt.plot_sysrem_cc_2D(
     cc_rvs=cc_rvs,
-    cc_values=cc_values,
+    cc_values=ccv_per_spec,
     mean_spec_lambdas=np.mean(waves,axis=1),
     planet_rvs=planet_rvs,
     plot_label=ss.label,)
 
-# Now plot the combined cross-correlation
-tplt.plot_sysrem_cc_2D(
-    cc_rvs=cc_rvs,
-    cc_values=cc_vals_total_subbed,
-    mean_spec_lambdas=None,
-    planet_rvs=planet_rvs,
-    fig_size=(6,6),
-    plot_label="comb_{}".format(ss.label),)
-
-assert False
-
-# Plot the Kp vs Vsys map
+#------------------------------------------------------------------------------
+# Kp-Vsys map
+#------------------------------------------------------------------------------
 Kp_steps, Kp_vsys_map = sr.compute_Kp_vsys_map(
     cc_rvs=cc_rvs,
-    cc_values=cc_vals_total_subbed[:,:,0,:],
+    cc_values=ccv_per_spec,
     transit_info=transit_info_list[ss.transit_i],
     syst_info=syst_info,
     Kp_lims=ss.Kp_lims,
     Kp_step=ss.Kp_step,)
 
-# Plot Kp-Vsys map
-tplt.plot_kp_vsys_map()
+tplt.plot_kp_vsys_map(
+    cc_rvs=cc_rvs,
+    Kp_steps=Kp_steps,
+    Kp_vsys_map=Kp_vsys_map,
+    mean_spec_lambdas=np.mean(waves,axis=1),
+    plot_label=ss.label,)

scripts_transit/test_sysrem.py
@@ -3,7 +3,6 @@
 import numpy as np
 import transit.utils as tu
 import transit.sysrem as sr
-from tqdm import tqdm
 import astropy.units as u
 import transit.plotting as tplt
 from astropy import constants as const
@@ -27,13 +26,22 @@ planet_rvs = \
 # Run Nik's SYSREM
 transit_i = 0
 spectra = fluxes_list[transit_i]
+sigmas = sigmas_list[transit_i]
 (n_phase, n_spec, n_px) = spectra.shape
 
-mm = np.isnan(spectra)
-spectra[mm] = np.nanmean(spectra)
+print("Cleaning datacube...")
+resid_init, flux, e_flux_init = sr.clean_and_compute_initial_resid(
+        spectra=spectra,
+        e_spectra=sigmas,
+        strong_telluic_mask=np.full_like(spectra, False),
+        mjds=transit_info_list[transit_i]["mjd_mid"].values,
+        sigma_threshold_phase=ss.sigma_threshold_phase,
+        sigma_threshold_spectral=ss.sigma_threshold_spectral,
+        do_normalise=False,)
 
-resid_all = sr.sysrem_piskunov(
-    spectra=spectra.copy(),
+print("Running SYSREM...")
+resid_all = sr._sysrem_piskunov(
+    spectra=resid_init,
     n_iter=ss.n_sysrem_iter,
     sigma_threshold=3.0,)
 
@@ -54,25 +62,10 @@ cc_rvs, ccv_per_spec, ccv_global = sr.cross_correlate_sysrem_resid(
     cc_rv_lims=ss.cc_rv_lims,
     interpolation_method="cubic",)
 
-# (n_phase, n_spec, n_cc)
-ccv_norm = ccv_per_spec.copy()
-ccvg = ccv_global.copy()
-(_, _, _, n_cc) = ccv_norm.shape
-
-# Normalise
-# TODO: move inside function
-for iter_i in range(ss.n_sysrem_iter+1):
-    for spec_i in tqdm(range(n_spec), desc="Plotting", leave=False):
-        # Sum along the CC direction
-        norm_1D = np.nansum(ccv_norm[iter_i, :,spec_i,:], axis=1)
-        norm_2D = np.broadcast_to(norm_1D[:,None], (n_phase, n_cc))
-
-        ccv_norm[iter_i, :,spec_i,:] /= norm_2D
-
 # Plot cross-correlation
 tplt.plot_sysrem_cc_2D(
     cc_rvs=cc_rvs,
-    cc_values=ccv_norm,
+    cc_values=ccv_per_spec,
     mean_spec_lambdas=np.mean(waves,axis=1),
     planet_rvs=planet_rvs,
     plot_label=ss.label,)
@@ -82,7 +75,7 @@ tplt.plot_sysrem_cc_2D(
 #------------------------------------------------------------------------------
 Kp_steps, Kp_vsys_map = sr.compute_Kp_vsys_map(
     cc_rvs=cc_rvs,
-    cc_values=ccv_norm,
+    cc_values=ccv_per_spec,
     transit_info=transit_info_list[ss.transit_i],
     syst_info=syst_info,
     Kp_lims=ss.Kp_lims,

transit/sysrem.py
@@ -21,7 +21,8 @@ def clean_and_compute_initial_resid(
     sigma_threshold_spectral=6.0,
     n_max_phase_bad_px=5,
     do_clip_spectral_dimension=False,
-    interpolation_method="cubic",):
+    interpolation_method="cubic",
+    do_normalise=True,):
     """Function to clean a spectral data cube prior to running SYSREM. We sigma
     clip along the phase dimension (i.e. the time series of each pixel) as well
     as along the spectral dimension. 
@@ -64,6 +65,11 @@ def clean_and_compute_initial_resid(
         ['linear', 'nearest', 'nearest-up', 'zero', 'slinear', 'quadratic',
         'cubic', 'previous', or 'next'].
 
+    do_normalise: boolean, default: True
+        Whether to normalise the cleaned + interpolated fluxes or not. This
+        should be set to False if intending to use the Piskunov detrending
+        algorithm.
+
     Returns
     -------
     resid_init: 3D float array
@@ -90,7 +96,8 @@ def clean_and_compute_initial_resid(
 
     # Loop over all spectral segments and spectral pixels
     for spec_i in range(n_spec):
-        for px_i in range(n_px):
+        desc = "Cleaning spectrum {}/{}".format(spec_i+1, n_spec)
+        for px_i in tqdm(range(n_px), desc=desc, leave=False):
             # Sigma clip along the phase dimension to compute the bad pixel
             # mask for px_i
             with warnings.catch_warnings():
@@ -166,19 +173,30 @@ def clean_and_compute_initial_resid(
     # Finally we pre-normalise the spectra. Per Birkby+2013, this involves:
     #   1) Dividing each individual *spectrum* by its mean value
     #   2) Subtracting unity
-    mean_2D = np.nanmean(flux, axis=2)  # [n_phase, n_spec]
-    mean_3D = np.broadcast_to(
-        mean_2D[:,:,None], flux.shape)  # [n_phase, n_spec, n_px]
-    resid_init = flux / mean_3D - 1
-    
-    # Also rescale uncertainties
-    e_flux /= mean_3D
+    if do_normalise:
+        mean_2D = np.nanmean(flux, axis=2)  # [n_phase, n_spec]
+        mean_3D = np.broadcast_to(
+            mean_2D[:,:,None], flux.shape)  # [n_phase, n_spec, n_px]
+        resid_init = flux / mean_3D - 1
+        
+        # Also rescale uncertainties
+        e_flux /= mean_3D
 
-    # Finally, for any remaining non-interpolated bad px, set the fluxes and
-    # uncertainties to default values
-    resid_init[bad_px_mask] = 0.0
-    flux[bad_px_mask] = 1.0
-    e_flux[bad_px_mask] = 1E20
+        # Finally, for any remaining non-interpolated bad px, set the fluxes
+        # and uncertainties to default values
+        resid_init[bad_px_mask] = 0.0
+        flux[bad_px_mask] = 1.0
+        e_flux[bad_px_mask] = 1E20
+
+        # No normalisation
+    else:
+        resid_init = flux.copy()
+
+        # Finally, for any remaining non-interpolated bad px, set the fluxes
+        # and uncertainties to default values
+        resid_init[bad_px_mask] = np.median(resid_init[~bad_px_mask])
+        flux[bad_px_mask] = np.median(flux[~bad_px_mask])
+        e_flux[bad_px_mask] = 1E20
 
     return resid_init, flux, e_flux
 
@@ -622,6 +640,15 @@ def cross_correlate_sysrem_resid(
             # Store
             ccv_global[sysrem_iter_i, :, :] = cc_val.T
 
+    # Normalise
+    for iter_i in range(n_sysrem_iter):
+        for spec_i in tqdm(range(n_spec), desc="Normalising", leave=False):
+            # Sum along the CC direction
+            norm_1D = np.nansum(ccv_per_spec[iter_i, :,spec_i,:], axis=1)
+            norm_2D = np.broadcast_to(norm_1D[:,None], (n_phase, n_rv_steps))
+
+            ccv_per_spec[iter_i, :,spec_i,:] /= norm_2D
+
     # All done, return our array of cross correlation results
     return cc_rvs, ccv_per_spec, ccv_global
 
@@ -701,12 +728,13 @@ def compute_Kp_vsys_map(
                 # Loop over all phases
                 for phase_i in range(n_phase):
                     # Grab relevant parameters from transit_info
-                    phase = transit_info.iloc[phase_i]["phase_mid"]
+                    # Note: phases have been negated.
+                    phase = -1 * transit_info.iloc[phase_i]["phase_mid"]
                     v_bcor = transit_info.iloc[phase_i]["bcor"]
                     v_star = syst_info.loc["rv_star", "value"]
 
                     # Compute the velocity shift between this phase and the
-                    # planet rest frame velocity.
+                    # planet rest frame velocity. TODO: check sign on bcor.
                     vp = Kp * np.sin(2*np.pi*phase) + v_star - v_bcor
 
                     # Create interpolator for the CC values of this phase